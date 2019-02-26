2e9ca3ce40ba27b748ea62f1896e6957667d6851
Pranudeep <36022737+pranudeep8@users.noreply.github.com>
2019-02-26 15:30:58 -0600

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,2 @@
+1. Clone/Download UXDevCodeChallenge repository.
+2. Navigate to project folder and open index.html on browser.
---
+++ js/javascript.js
@@ -0,0 +1,25 @@
+
+$('#blueberries').click(function() {
+    $('#blueberries #output').html(function(i, val) { return val*1+1 });
+});
+
+$('#orange-chair').click(function() {
+    $('#orange-chair #output').html(function(i, val) { return val*1+1 });
+});
+
+$('#leather-chair').click(function() {
+    $('#leather-chair #output').html(function(i, val) { return val*1+1 });
+});
+
+$('#potted-plant').click(function() {
+    $('#potted-plant #output').html(function(i, val) { return val*1+1 });
+});
+
+$('#camera').click(function() {
+    $('#camera #output').html(function(i, val) { return val*1+1 });
+});
+
+$('#wood-stool').click(function() {
+    $('#wood-stool #output').html(function(i, val) { return val*1+1 });
+});
+
